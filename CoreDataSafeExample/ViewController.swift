b5fec380282557d86b01bc98492b926aeeb411c6
Eric Elfner <eje@geoderanch.com>
2016-01-31 09:06:19 -0500

    Initial Commit

---
+++ CoreDataSafeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataSafeExample
+//
+//  Created by Eric Elfner on 2016-01-31.
+//  Copyright © 2016 Eric Elfner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
