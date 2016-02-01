1736966884aebf6593ec6a112a600c9f2d13d008
Eric Elfner <eje@geoderanch.com>
2016-02-01 12:31:59 -0500

    First pass with working NSFetchResultsController

--- CoreDataSafeExample/ViewController.swift
+++ CoreDataSafeExample/ViewController.swift
@@ -7,42 +7,101 @@
 //
 
 import UIKit
+import CoreData
 
 private let kTestAuthors = ["Joan Slater", "William Newman", "Ella Nash", "Penelope Bower", "Neil Cameron"]
 private let kTestBooksWords = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum".characters.split(" ").map(String.init)
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate {
 
     private let cdSafe = CoreDataSafe(dbName: "Books")
-    private var authors : [Author]!
+    //private var authors : [Author]!
+
+    @IBOutlet weak var tableView:UITableView!
+    
+    lazy private var fetchedResultsController: NSFetchedResultsController = self.currentFetchResultsController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        authors = createTestAuthors()
+        //authors = createTestAuthors()
+        try! fetchedResultsController.performFetch()
     }
 
+    // MARK: - UITableViewDelegate and DataSource
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return authors.count
+        if let sections = fetchedResultsController.sections {
+            return sections.count
+        }
+        return 0
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let books = authors[section].books {
-            return books.count
+        if let sections = fetchedResultsController.sections {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
         }
-        else {
-            return 0
-        }
+        return 0
     }
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let author = authors[section]
-        return (author.name ?? "") + " - \(author.books!.count)"
+        if let sections = fetchedResultsController.sections {
+            let currentSection = sections[section]
+            return currentSection.name
+            //return (author.name ?? "") + " - \(author.books!.count)"
+        }
+        
+        return nil
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let book = authors[indexPath.section].books![indexPath.row] as! Book
         let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
+        configureCell(cell, indexPath:indexPath)
+        return cell
+    }
+    func configureCell(cell:UITableViewCell, indexPath: NSIndexPath) {
+        let book = fetchedResultsController.objectAtIndexPath(indexPath) as! Book
+        
         cell.textLabel?.text = book.title
         cell.detailTextLabel?.text = "\(book.pageCount!)pp: " + (book.comment ?? "")
-        return cell
+    }
+    
+    // MARK: - NSFetchedResultsControllerDelegate
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        tableView.endUpdates()
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        if let indexPath = newIndexPath {
+            switch (type) {
+            case .Insert: tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            case .Delete: tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            case .Update:
+                let cell = tableView.cellForRowAtIndexPath(indexPath)!
+                configureCell(cell, indexPath:indexPath)
+            case .Move:
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+                
+                if let newIndexPath = newIndexPath {
+                    tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Fade)
+                }
+            }
+        }
+    }
+    
+    private func currentFetchResultsController() -> NSFetchedResultsController {
+        let fetchRequest = NSFetchRequest(entityName:"Book")
+        let sortDescriptor = NSSortDescriptor(key:"author.name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest:fetchRequest, managedObjectContext:self.cdSafe.mainMoc, sectionNameKeyPath:"author.name", cacheName:nil)
+        
+        fetchedResultsController.delegate = self
+        
+        return fetchedResultsController
     }
     
 }
